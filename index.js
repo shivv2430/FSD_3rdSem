5d30fed4e014e9e9cf441c9c1a9cc72766364e52
Shivani Pal <2430.shivanipal@gmail.com>
2026-08-19 19:46:54 +0530

    first class(19-8-26)

---
+++ index.js
@@ -0,0 +1,56 @@
+// console.log("Heloo World");
+
+// let age = 20;
+
+// if (age >= 18) {
+//     console.log("Eligible");
+// } else {
+//     console.log("Not Eligible");
+// }
+
+//Nested loop
+// graduate = "YES";
+// branch = "CSE";
+
+// if (graduate == "YES" && branch == "EE") {
+
+//     console.log("Master");
+// } else {
+//     console.log("Not ");
+// }
+
+//For loop
+// for (let i = 1; i <= 3; i++) {
+//     console.log(i);
+// }
+
+
+//for(Nested Loop) : 
+// for (let i = 0; i <= 3; i++) {
+//     let row = " ";
+//     for (let j = 0; j <= i; j++) {
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+//function
+// function greet() {
+//     console.log("Hello");
+// }
+
+// greet()
+
+//arrow function
+// var vote = (age) => {
+//     if (age >= 18) {
+//         return "Eligible";
+//     } else {
+//         return "Not Eligible";
+//     }
+// }
+
+// console.log(vote(20));
+
+//homework : 
+// array map(), filter(), reduce(), forEach()
